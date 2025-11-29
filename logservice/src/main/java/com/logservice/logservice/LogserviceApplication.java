ef48eeac1bbff104852e538c8250a9399632ed66
wingorithm <erwin.gunawan@binus.ac.id>
2025-11-29 23:41:08 +0700

    change deprecated feign client

--- logservice/src/main/java/com/logservice/logservice/LogserviceApplication.java
+++ logservice/src/main/java/com/logservice/logservice/LogserviceApplication.java
@@ -2,7 +2,7 @@ package com.logservice.logservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
--- logservice/src/main/java/com/logservice/logservice/client/ServiceAClient.java
+++ logservice/src/main/java/com/logservice/logservice/client/ServiceAClient.java
@@ -1,7 +1,7 @@
 package com.logservice.logservice.client;
 
 import com.logservice.logservice.dto.BookResponse;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
