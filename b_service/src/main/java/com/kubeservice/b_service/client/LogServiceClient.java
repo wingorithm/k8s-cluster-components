63f1f550e293b822e1ee48c3369e2667c9bf7b60
wingorithm <erwin.gunawan@binus.ac.id>
2025-11-30 02:05:10 +0700

    add istio gateway and http routing rule for each service

--- b_service/src/main/java/com/kubeservice/b_service/client/LogServiceClient.java
+++ b_service/src/main/java/com/kubeservice/b_service/client/LogServiceClient.java
@@ -9,7 +9,7 @@ import java.util.List;
 @FeignClient(value = "logservice", url = "${url.logservice}")
 public interface LogServiceClient {
 
-    @GetMapping(value = "/api/books")
+    @GetMapping(value = "/api/logservice/books")
     List<BookResponse> getBookFromFriend();
 
 }
--- logservice/src/main/java/com/logservice/logservice/controller/BookController.java
+++ logservice/src/main/java/com/logservice/logservice/controller/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/logservice/books")
 @RequiredArgsConstructor
 public class BookController {
 
