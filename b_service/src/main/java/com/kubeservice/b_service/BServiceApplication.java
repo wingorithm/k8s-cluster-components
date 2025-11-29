c3c35edc3c8f937cca07aa6c9146c5cd89d85b89
wingorithm <erwin.gunawan@binus.ac.id>
2025-11-30 00:06:01 +0700

    init

---
+++ b_service/src/main/java/com/kubeservice/b_service/BServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kubeservice.b_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BServiceApplication.class, args);
+	}
+
+}
