af6b9e7fccf1c66a9aef3b28506f15d59902ac50
wingorithm <erwin.gunawan@binus.ac.id>
2025-11-29 22:45:43 +0700

    init

---
+++ a_service/src/test/java/com/kubeservice/a_service/AServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubeservice.a_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
